c2c8534966c0b523fa720c1dffca50166e313851
DASG555 <alexandergarrido555@gmail.com>
2022-08-29 21:36:48 -0600

    Pagina WordPress V1

---
+++ test.php
@@ -0,0 +1,12 @@
+<?php 
+
+$entero = 5;
+
+if ($entero >= 2){
+    echo "El número es mayor a 2";
+    }
+    else{
+        echo "El número es menor a 2";
+    }
+?>
+
---
+++ wp-content/plugins/wp-and-divi-icons/blocks/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill', 'wp-rich-text'), 'version' => '6dc23e04798ca2b32430bde6868ab9c1');
